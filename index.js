8d3addd9d7cd29bec3f55f15af46d7fa10e087c1
SwalihaHaque <swalihahaque59@gmail.com>
2023-09-21 01:23:21 +0600

    added Cupon Code Discount

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let total = 0;
 function clickMe(target) {
     const container = document.querySelector("#selected-item");
-    //console.log(target.parentNode.childNodes[3].innerText);
     const itemName = target.parentNode.childNodes[3].innerText;
 
     const li = document.createElement("li");
@@ -10,12 +9,44 @@ function clickMe(target) {
     container.appendChild(li);
 
     //display price and sum of the total price
-
-    console.log(target.parentNode.childNodes[5].innerText.split(" ")[1]);
-    const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
+    const price = target.parentNode.childNodes[5].innerText;
     total = parseInt(total) + parseInt(price);
-    document.getElementById("total").innerText = total;
+    document.getElementById("total-price").innerText = total;
+
+    //button enable
+    if (total > 0) {
+        const makePurchaseBtn = document.getElementById("make-purchase");
+        makePurchaseBtn.removeAttribute('disabled');
+    }
+    if (total >= 200) {
+        const applyBtn = document.getElementById("apply-btn");
+        applyBtn.removeAttribute('disabled');
+    }
 };
+
+// coupon apply
+function couponApply() {
+    let validPromoCode = "SELL200";
+
+    let couponFieldValue = document.getElementById("coupon-field").value;
+    let totalValue = parseFloat(document.getElementById("total-price").innerText);
+    let discountField = document.getElementById("discount-price");
+    let grandTotalField = document.getElementById("total");
+
+    if (couponFieldValue === validPromoCode) {
+
+        let discountValue = totalValue * 0.2;
+        console.log(discountValue);
+
+        discountField.innerText = discountValue.toFixed(2);
+        grandTotalField.innerText = discountValue;
+        let grandTotalValue = totalValue - discountValue;
+        grandTotalField.innerText = grandTotalValue.toFixed(2);
+    }
+};
+
+
+
 
 
 
